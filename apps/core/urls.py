c4a4541915672a8e6b8903d38315de87001daead
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-08-10 23:27:33 +0300

    home-page

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from .views import home
+from .views import *
 
 urlpatterns = [
     path("", home, name="home"),
+    path("services/", services, name="services"),
+    path("booking/", booking, name="booking"),
+    path("about/", about, name="about"),
+    path("reviews/", reviews, name="reviews"),
+    path("contact/", contact, name="contact"),
 ]
+
--- apps/core/views/__init__.py
+++ apps/core/views/__init__.py
@@ -1,1 +1,7 @@
-from .home import home
+from .home import*
+from .home import *
+from .services import *
+from .booking import *
+from .about import *
+from .reviews import *
+from .contact import *
